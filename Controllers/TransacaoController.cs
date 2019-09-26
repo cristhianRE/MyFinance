0b9362fe2d794ffb03ae244228e2bd4c3c313e1d
Cristhian Ebert <cristhianREbert@gmail.com>
2019-09-26 09:44:12 -0300

    Registro de Transações Financeiras pt 1

--- Controllers/TransacaoController.cs
+++ Controllers/TransacaoController.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyFinance.Models;
 
 namespace MyFinance.Controllers
 {
     public class TransacaoController : Controller
     {
+        IHttpContextAccessor HttpContextAccessor;
+
+        public TransacaoController(IHttpContextAccessor httpContextAccessor)
+        {
+            HttpContextAccessor = httpContextAccessor;
+        }
         public IActionResult Index()
         {
+            TransacaoModel objTransacao = new TransacaoModel(HttpContextAccessor);
+            ViewBag.ListaTransacao = objTransacao.ListaTransacao();
             return View();
         }
 
